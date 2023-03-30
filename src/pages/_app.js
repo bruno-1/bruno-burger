6e5a97e143544091443a873f876fb37341830cde
Bruno Serafini <bruno_benaglia2@outlook.com>
2023-03-30 20:56:05 -0300

    starting the project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import Layout from '@/components/layout'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Bruno Burger</title>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
